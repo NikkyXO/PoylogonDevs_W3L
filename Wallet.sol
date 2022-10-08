8915104beaf1d6bcd940bf67140b9c1fe40b2a98
NikkyXO <alexisdame2017@gmail.com>
2022-10-08 01:47:09 +0100

    fixed errors

---
+++ Wallet.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+// Assignment
+// Build a wallet application that allows any user to
+//store, send and deposit ether.
+// Features:
+// Custom Error Messages
+// Use of events after key operations
+// Hint: Contract should be able to receive
+//  and use a modifier to enforce ownership
+// Submission Link : https://forms.gle/g6yHZm7pwsmF811N6
--- index.sol
+++ index.sol
@@ -1,37 +1,43 @@
+// SPDX-License-Identifier: MIT
 // Solidity program to implement
-// the above approach
+// Assignment
+// Using Remix Development tools or Hardhat
+// Development write a smart contract that
+// records the database of students in a class.
+// Hint the knowledge of struct, mapping, and
+// variable data type will be needed, The expected functions are
+// A function that allows you to register a student,
+// A function that allows you to see the record of a student using
+//the student Id(The id can be any data type you want).
+// You can also add as many functions as needed depending on your creativity.
+
 pragma solidity >=0.7.0 <0.9.0;
 
 contract StudentRecords {
-    // Create a structure for
-    // student details
     struct Student {
-        //uint student_id;
-        address ID;
+        uint256 student_id;
         string fName;
         string lName;
     }
-    // Student[] public studentList;
+
+    Student[] public studentList;
 
     mapping(address => Student) public students;
-    // mapping(uint256 => Student) public students;
-    uint256 public playerCount = 0;
 
-    function AddStudent(string memory lName, string memory fName) public {
-        // FOR AN ARRAY
-        // studentList.push(Student(fName, lName));
+    uint256 public StudentCount = 1;
 
-        // FOR A STRUCT
-        // playerCount += 1;
-        // students[student_id].student_id = playerCount
-        // students[student_id].fName = fName;
-        // students[student_id].lName = lName;
+    Student public newStudent;
 
-        students[msg.sender] = Student(msg.sender, fName, lName);
-        playerCount += 1;
+    constructor(string memory _lName, string memory _fName) {
+        newStudent = Student(StudentCount, _fName, _lName);
+        // newStudent.fName = _fName;
+        // newStudent.lName = _lName;
+        // newStudent.student_id = StudentCount;
+        StudentCount += 1;
+        studentList.push(newStudent);
     }
 
-    function getStudent(address ID) public view returns (Student memory) {
-        return students[ID];
+    function getStudent(address stdntaddress) public view returns (uint256) {
+        return students[stdntaddress].student_id;
     }
 }
